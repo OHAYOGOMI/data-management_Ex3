518f7afe948b46855be49c478ca3ff574e8fc3b9
OHAYOGOMI <rusetto08@icloud.com>
2025-07-03 23:25:09 +0900

    Won/lost message added

--- ex3.cpp
+++ ex3.cpp
@@ -7,11 +7,20 @@ int main() {
     
     int a = rand() % 6 + 1;
     int b = rand() % 6 + 1;
+    int c = a+b;
 
     cout << "Rolling dice..."<< endl;
     cout << "Die 1: "<<a<< endl;
     cout << "Die 2: "<<b<< endl;
-    cout << "Total value: "<<a+b<< endl;
+    cout << "Total value: "<<c<< endl;
+
+    if( c > 7 ){
+        cout << "You won" << endl;
+    }
+    
+    if( c <= 7 ){
+        cout << "You lost" << endl;
+    }
 
     return 0;
 }
